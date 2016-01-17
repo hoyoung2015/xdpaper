7a708ffd73fb80797705335d54eecf66e2410c27
hoyoung <hoyoung>
2016-01-17 22:28:38 +0800

    f

---
+++ Application/User/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', '8jbD.kc>VieU<2u_7!LP~H-C]An)R94q$0t#M&W+'); //加密KEY
+define('UC_DB_DSN', 'mysqli://root:@127.0.0.1:3306/xdpaper'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', ''); // 数据表前缀，使用Model方式调用API必须配置此项
